ffe5e63360875fd3e8c5da4f8a026981543d08f4
Flowdawan <flo-m@gmx.at>
2022-09-08 14:52:31 +0200

    add aditional check

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,31 +3,44 @@ import * as React from 'react';
 import './Card.css';
 import Pokemon from './Pokemon';
 
+const uncovered = false;
+
 export default function Card() {
     const glumanda = new Pokemon('Glumanda', 'placeholder.png', '3.3', '6000', '70', '20', '15');
-    return (
-    <div className="card">
-        <h1>{ glumanda.name }</h1>
-        <img src={`${process.env.PUBLIC_URL}/${glumanda.image}`} alt="glumanda.name" height="200" width="200" />
-        <table>
-            <tbody>
-                {Object.keys(Pokemon.properties).map((property) => {
-                    const pokemonProperty = Pokemon.properties[property];
-                    return (
-                        <tr key={property}>
-                            <td>
-                                {pokemonProperty.label}
-                            </td>
-                            <td>
-                                {glumanda[property]}
-                                &nbsp;
-                                {pokemonProperty.unit}
-                            </td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-    </div>
-  );
+    const front = (
+        <div className="card">
+            <h1>{ glumanda.name ? glumanda.name : 'Unbekannt' }</h1>
+            {glumanda.image && (
+            <img src={`${process.env.PUBLIC_URL}/${glumanda.image}`} alt="glumanda.name" height="200" width="200" />
+            )}
+            <table>
+                <tbody>
+                    {Object.keys(Pokemon.properties).map((property) => {
+                        const pokemonProperty = Pokemon.properties[property];
+                        return (
+                            <tr key={property}>
+                                <td>
+                                    {pokemonProperty.label}
+                                </td>
+                                <td>
+                                    {glumanda[property]}
+                                    &nbsp;
+                                    {pokemonProperty.unit}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+
+    const back = (
+        <div className="card back" />
+    );
+
+    if (uncovered) {
+        return front;
+    }
+    return back;
 }
